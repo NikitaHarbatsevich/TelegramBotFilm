98586ee0811d7cdf9a403c145d6c613f6d602840
NikitaHarbatsevich <varianrin63@gmail.com>
2023-06-14 18:21:24 +0300

    fix

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ HEADERS = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 BASE_URL = "https://kinokrad.cc"
 
 all_films = []
-
-
-
-
+db_list = []
 
 
 def parce_all_films():
     response = requests.get(BASE_URL)
     soup = BeautifulSoup(response.content, "html.parser")
     items = soup.find_all("div", class_ = "shorbox")
+
+    connect = sqlite3.connect("user.db")
+    cursor = connect.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS movie(movie_title STR, link STR, year_of_issue STR, image STR)""")
+    connect.commit()
+
     for item in items:
 
         all_films.append(
@@ -29,15 +33,14 @@ def parce_all_films():
             }
         )
 
-    connect = sqlite3.connect("user.db")
-    cursor = connect.cursor()
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS movie(movie_title STR, link STR, year_of_issue STR, image STR)""")
-    connect.commit()
+        user_list = [item.find("a").text, item.find("a").get("href"), item.find("div", class_ = "item year").text.__str__().split("\n")[2], f"https://kinokrad.cc{item.find('div', class_ = 'postershort').find('img').get('data-src')}"]
+        cursor.execute("INSERT INTO movie VALUES(?, ?, ?, ?);", user_list)
+        connect.commit()
 
-    user_list = item
-    cursor.execute("INSERT INTO movie VALUES(?, ?, ?, ?);", user_list)
-    connect.commit()
+    for value in cursor.execute("SELECT * FROM movie"):
+        db_list.append(value)
+
 
 parcer = parce_all_films()
 
@@ -47,21 +50,15 @@ bot = telebot.TeleBot("6053934657:AAGIRYaXbzD5kbdTOsdl6exoURR_qJq71pw")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Выбрать фильм по названию")
-    btn2 = types.KeyboardButton("Предложить случайный фильм(не просматриваемый)")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id, text=f"Привет, {message.from_user.first_name} {message.from_user.last_name}!, меня зовут TeleFilm bot.".format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=f"Привет, {message.from_user.first_name} {message.from_user.last_name}!, меня зовут TeleFilm bot, пишите название фильма.".format(message.from_user))
 
 @bot.message_handler(content_types=['text'])
 def func(message):
-    if (message.text == "Выбрать фильм по названию"):
-        bot.send_message(message.chat.id, text="Пишите название (все в нижнем регистре)")
+    for i in db_list:
+        if message.text in i[0]:
+            bot.send_message(message.chat.id, text=f"Название фильма: {i[0]}\nСсылка: {i[1]}\nГод выпуска: {i[2]}\nКартинка: {i[3]}")
 
-
-
-
-
+print(db_list)
 
 
 bot.polling(none_stop=True)
